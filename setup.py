23c5cdb0f6c046686f168e2b388ebbf19252b580
neerajbafila@gmail.com <neerajbafila@gmail.com>
2021-09-09 16:48:43 +0530

    workflow updated-1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Admission prediction", 
+    author="Neeraj", 
+    packages=find_packages(),
+    license="MIT"
+)
