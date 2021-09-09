725f4edaa6fde5da23adc4eff5bbb97d392e1385
neerajbafila@gmail.com <neerajbafila@gmail.com>
2021-09-09 14:59:17 +0530

    pytest updated

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,22 +12,17 @@ class NotInRange(Exception):
         super().__init__(self.message)
 
 
-class MinValue(Exception):
-    def __init__(self, message="Less than min value required"):
-        self.message = message
-        super().__init__(self.message)
+# class MinValue(Exception):
+#     def __init__(self, message="Less than min value required"):
+#         self.message = message
+#         super().__init__(self.message)
 
 
 
 def test_check_value_range():
-    a = 5
+    a = 15
     with pytest.raises(NotInRange):
         if a not in range(10,20):
             raise NotInRange
 
 
-def test_check_min_value():
-    a = 5
-    with pytest.raises(NotInRange):
-        if a < 10:
-            raise MinValue
