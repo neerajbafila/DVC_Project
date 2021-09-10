b4296677cc02a82483ea33f377429d2e124c7cbe
neerajbafila@gmail.com <neerajbafila@gmail.com>
2021-09-10 20:38:25 +0530

    test cases are added in prediction.py

--- application_v1.py
+++ application_v1.py
@@ -8,8 +8,9 @@ from scipy.sparse import data
 import yaml
 import joblib
 import numpy as np
-from prediction_service import predict
 
+
+params_path = "params.yaml"
 webapp_root = "webapp"
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
@@ -18,6 +19,25 @@ template_dir = os.path.join(webapp_root, "templates")
 application = Flask(__name__, static_folder=static_dir,template_folder=template_dir) # initializing a flask app
 # app=application
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+        return config
+
+def predict(gre_score,toefl_score,university_rating,sop,lor,cgpa,research):
+    try:
+        config = read_params(params_path)
+        model_dir_path = config['webapp_model_dir']
+        scaler_dir_path = config['webapp_scaler_dir']
+        model = joblib.load(model_dir_path)
+        scaler = joblib.load(scaler_dir_path)
+        data = scaler.transform([[gre_score,toefl_score,university_rating,sop,lor,cgpa,research]])
+        # data = scaler.fit_transform([gre_score,toefl_score,university_rating,sop,lor,cgpa,research])
+        prediction = model.predict(data)
+        print(prediction)
+        return prediction
+    except Exception as e:
+            print(e)
 
 @application.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
@@ -30,26 +50,18 @@ def index():
     if request.method == 'POST':
         try:
             #  reading the inputs given by the user
-            GRE_Score=float(request.form['gre_score'])
-            TOEFL_Score = float(request.form['toefl_score'])
-            University_Rating = float(request.form['university_rating'])
-            SOP = float(request.form['sop'])
-            LOR = float(request.form['lor'])
-            CGPA = float(request.form['cgpa'])
+            gre_score=float(request.form['gre_score'])
+            toefl_score = float(request.form['toefl_score'])
+            university_rating = float(request.form['university_rating'])
+            sop = float(request.form['sop'])
+            lor = float(request.form['lor'])
+            cgpa = float(request.form['cgpa'])
             is_research = request.form['research']
             if(is_research=='yes'):
                 research=1
             else:
                 research=0
-            data_dic = {"GRE_Score": GRE_Score,
-                        "TOEFL_Score": TOEFL_Score,
-                        "University_Rating": University_Rating,
-                        "SOP":SOP,
-                        "LOR": LOR,
-                        "CGPA":CGPA,
-                        "Research": research}
-            prediction = predict.form_response(data_dic)
-            # prediction = predict(gre_score,toefl_score,university_rating,sop,lor,cgpa,research)
+            prediction = predict(gre_score,toefl_score,university_rating,sop,lor,cgpa,research)
             # filename = 'finalized_model.pickle'
             # loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
             # # predictions using the loaded model file
@@ -59,8 +71,7 @@ def index():
             return render_template('results.html',prediction=np.round(100*prediction[0][0],4))
         except Exception as e:
             print('The Exception message is: ',e)
-            # return 'something is wrong'
-            return str(e)
+            return 'something is wrong'
     # return render_template('results.html')
     else:
         return render_template('index.html')
---
+++ prediction_service/predict.py
@@ -0,0 +1,84 @@
+import os
+from flask.wrappers import Response
+import yaml
+import joblib
+import numpy as np
+import json
+
+params_path = "params.yaml"
+schema_path = os.path.join("prediction_service", "schema.json")
+
+class NotinRange(Exception):
+    """[If user sending value which are not in range as per dataset]
+
+    Args:
+        Exception ([type]): [description]
+    """
+    def __init__(self, message="Gaiven value are not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NotinColumn(Exception):
+    """[if user sending request from the
+        from tool like postman in that case he might add some exta column]
+
+    Args:
+        Exception ([type]): []
+    """
+    def __init__(self, message="Not in columnn"):
+        self.message = message
+        super().__init__(self, message)
+
+
+def read_params(config_path=params_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+        return config
+
+def predict_y(dic_data):
+    print(dic_data.values())
+    data = np.array([list(dic_data.values())])
+    config = read_params(config_path=params_path)
+    model_dir_path = config['webapp_model_dir']
+    scaler_dir_path = config['webapp_scaler_dir']
+    model = joblib.load(model_dir_path)
+    scaler = joblib.load(scaler_dir_path)
+    scaled_data = scaler.transform(data)
+    prediction = model.predict(scaled_data)
+    
+    """[restricting prediction range between 0 to 100]
+    """
+    try:
+        if 0 <= prediction <=100:
+            return prediction
+        else:
+            raise NotinRange
+    except NotinRange:
+        return "Unexcepted error occurred"
+
+def get_schema(schema_path= schema_path):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+        return schema
+
+def validate_input(dic_data):
+    
+    # def validate_cols(col): #for API input
+    #     schema = get_schema()
+    #     actual_col = schema.keys()
+    #     if col not in actual_col:
+    #         raise NotinColumn
+    def validate_values(col, val):
+        schema = get_schema()
+        if not (schema[col]["min"] <= float(dic_data[col]) <=schema[col]["max"]):
+            raise NotinRange
+    for col, val in dic_data.items():
+        # validate_cols(col)
+        validate_values(col, val)
+    return True
+
+def form_response(dic_data):
+    if validate_input(dic_data):
+        response = predict_y(dic_data)
+        return response
